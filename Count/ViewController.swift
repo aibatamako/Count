6f1e3ce8441a0fc81df533bf373b254fe25728bb
Tamako Aiba <t.aiba@4nonome.com>
2021-06-26 01:38:16 +0900

    追加課題カウント完成。

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var countLabel: UILabel!
+    @IBOutlet var parityLabel: UILabel!
+    
+    var number: Int = 0
+    var parity: String = "偶数"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func checkParity(x:Int) -> String{
+        
+        if x % 2 == 0{
+            parity = "偶数"
+        }else {
+            parity = "奇数"
+        }
+        return parity
+    }
+    
+    @IBAction func plus() {
+        number += 1
+        countLabel.text = String(number)
+        parityLabel.text = checkParity(x: number)
+    }
+    
+    
 
 }
 
